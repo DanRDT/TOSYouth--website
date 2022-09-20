3aaa06d25fc71f47e9298ea3fab42cad825b1278
unknown <pulberdan@gmail.com>
2022-09-20 16:15:45 -0400

    header

---
+++ scripts/navigation.js
@@ -0,0 +1,18 @@
+const navigationButton = document.querySelector('#menu-button')
+const navigation = document.querySelector('nav')
+
+ 
+
+export default navigationButton.addEventListener('click', () => {
+    const visibility = navigation.getAttribute('data-visible')
+
+    if (visibility === "false") {
+        navigation.setAttribute("data-visible", "true")
+        navigationButton.setAttribute("data-active", "true")
+    }
+    else if (visibility === "true") {
+        navigation.setAttribute("data-visible", "false")
+        navigationButton.setAttribute("data-active", "false")
+    }
+
+})
