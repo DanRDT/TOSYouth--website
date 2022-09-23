7ef7a87b669d001c88be93238b7e9e2d65d50888
unknown <pulberdan@gmail.com>
2022-09-22 22:30:37 -0400

    appear on scroll functionality

---
+++ scripts/appear-on-scroll.js
@@ -0,0 +1,25 @@
+const observedItems = document.querySelectorAll('[data-appear-on-scroll]')
+
+const options = {
+    threshold: 0,
+    rootMargin: "-30px"
+};
+
+const observer = new IntersectionObserver( (items, observer) => {
+    items.forEach(item => {
+        if (!item.isIntersecting) {
+          return;
+        }
+        else if (item.isIntersecting) {
+          item.target.setAttribute('data-appear-on-scroll', "true");
+          observer.unobserve(item.target);
+        }
+    })
+}, options)
+
+
+
+export default observedItems.forEach(item => {
+    observer.observe(item);
+    console.log(item)
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,5 @@
+// Navigation Slide-out Functionality
 import navigation from "./navigation.js"
+
+// Introspection observer to animate in items on scroll
+import appearOnScroll from "./appear-on-scroll.js"
