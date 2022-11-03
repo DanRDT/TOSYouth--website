0a0f14e5908653f67c8785b2d0b5797de9777e5f
unknown <pulberdan@gmail.com>
2022-11-02 21:39:46 -0400

    billing button

--- static/scripts/billing-info.js
+++ static/scripts/billing-info.js
@@ -1,12 +1,15 @@
 const billing = document.querySelector('.billing-info');
 const checkbox = document.querySelector('.same-as-shipping');
+const container = document.querySelector('.same-as-shipping-container');
 
 function checkCheckbox() {
     if (checkbox.checked) {
         billing.classList.add('same-as')
+        container.classList.add('same-as')
     }
     else if (!checkbox.checked) {
         billing.classList.remove('same-as')
+        container.classList.remove('same-as')
     } 
 }
 
