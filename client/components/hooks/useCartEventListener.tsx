a13368ad9c181a43e87a92891c34b3be67194a2c
unknown <pulberdan@gmail.com>
2022-11-15 23:15:48 -0500

    checkout totals

--- client/components/hooks/useCartEventListener.tsx
+++ client/components/hooks/useCartEventListener.tsx
@@ -4,10 +4,9 @@ import getCart from "./getCart";
 export default function useCartEventListener(setCart) {
     useEffect(() => {
         // update cart if it changes in another tab
-        function storageEventHandler(event) {
+        const storageEventHandler = (event) => {
             if (event.key === "ShoppingCart") {
                 setCart(getCart());
-                console.log("cart changed");
             }
         }
         window.addEventListener("storage", storageEventHandler);
--- client/components/merch/checkout/summary.tsx
+++ client/components/merch/checkout/summary.tsx
@@ -1,17 +1,68 @@
+import { useEffect, useRef, useState } from "react"
+import checkForShoppingCart from "../../hooks/checkForShoppingCart"
+import getCart from "../../hooks/getCart"
 import setLocalCheckoutInfo from "../../hooks/setLocalCheckoutInfo"
+import useCartEventListener from "../../hooks/useCartEventListener"
 
 const Summary = ({billingInfo, shippingInfo}) => {
+    const [checkoutInfoValid, setCheckoutInfoValid] = useState(true)
+    const [checkoutInfoValidCss, setCheckoutInfoValidCss] = useState('')
+    
+    const [cart, setCart] = useState([]);
+    const [subtotal, setSubtotal] = useState('')
+    const [tax, setTax] = useState('')
+    const [total, setTotal] = useState('')
+
+    useEffect(() => {
+        checkForShoppingCart();
+        setCart(getCart());
+    }, []);
+
+    useEffect(() => {
+        let currentSubtotal = 0;
+        cart.map((cartItem)=>{
+            currentSubtotal = currentSubtotal + (Number(cartItem.price) * Number(cartItem.quantity))
+        })
+        setSubtotal("$" + currentSubtotal.toFixed(2))
+        let tax = currentSubtotal * 0.07;
+        setTax("$" + (tax).toFixed(2))
+        setTotal("$" + (currentSubtotal+tax).toFixed(2))
+    }, [cart]);
+
+    useCartEventListener(setCart)
+
+    function infoNotValid() {
+        setCheckoutInfoValidCss("checkout-info-not-valid")
+        setTimeout(() => {
+            setCheckoutInfoValidCss('')
+        }, 2200);
+    }
 
     return (
         <>      
             <section className="summary">
-                <h4 className="subtotal">Subtotal: <span>$99.98</span></h4>
+                <h4 className="subtotal">Subtotal: <span>{subtotal}</span></h4>
                 <h4 className="shipping">Shipping: <span className="rgt-item"> Free </span>
                 </h4>
                 <p>Estimated Delivery: 5-7 business days</p>
-                <h4 className="tax">Tax: <span className="rgt-item">$6.07</span></h4>
-                <h4 className="total">Total: <span className="rgt-item">$105.05</span></h4>
-                <a className="pay-btn" onClick={() => setLocalCheckoutInfo(shippingInfo, billingInfo)}><h4>Proceed to Payment</h4></a>
+                <h4 className="tax">Tax: <span className="rgt-item">{tax}</span></h4>
+                <h4 className="total">Total: <span className="rgt-item">{total}</span></h4>
+                { checkoutInfoValid ? 
+                    (<a className="pay-btn" 
+                        onClick={() => {
+                            setLocalCheckoutInfo(shippingInfo, billingInfo)
+                        }}>
+                        <h4>Proceed to Payment</h4>
+                    </a>)
+                :
+                    (<a className={`pay-btn ${checkoutInfoValidCss}`} 
+                        onClick={() => {
+                            setLocalCheckoutInfo(shippingInfo, billingInfo)
+                            infoNotValid()
+                        }}>
+                        <h4>Proceed to Payment</h4>
+                    </a>)
+                }
             </section>
         </>
     )
