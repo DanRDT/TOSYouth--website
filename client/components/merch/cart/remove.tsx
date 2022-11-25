d327bcff8b92a04592ed56d4aa1b357a0c0fb883
unknown <pulberdan@gmail.com>
2022-11-25 00:28:40 -0500

    cart remove button bug

--- client/components/merch/cart/remove.tsx
+++ client/components/merch/cart/remove.tsx
@@ -1,12 +1,19 @@
+import CartItems from "./cart";
+
 const Remove = ({item, cart, setCart}) => {
 
-    const removeItem = (id,size,color) => {
+    const removeItem = (id,size,color,variant_id) => {
+        
         const newCart = [];
         cart.map((cartItem) => {
             if (cartItem.id != id || cartItem.size != size || cartItem.color != color) {
-                newCart.push(item)
+                newCart.push(cartItem)
             }
+            // if (variant_id != cartItem.variant_id) {
+            //     newCart.push(cartItem)
+            // }
         })
+        
         setCart(newCart);
         localStorage.setItem("ShoppingCart", JSON.stringify(newCart))
     }
@@ -14,7 +21,7 @@ const Remove = ({item, cart, setCart}) => {
 
     return (
         <>      
-            <p onClick={() => {removeItem(item.id, item.size, item.color)}}>Remove</p> 
+            <p onClick={() => {removeItem(item.id, item.size, item.color, item.variant_id)}}>Remove</p> 
         </>
     )
 }
