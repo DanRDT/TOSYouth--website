e2da498628d0f72eb2d1441265c0669df8f50d70
unknown <pulberdan@gmail.com>
2022-12-31 19:07:38 -0500

    minor fixes for deployment

--- client/pages/api/printify.tsx
+++ client/pages/api/printify.tsx
@@ -1,9 +1,10 @@
 
 export default async function handler(req, res) {
-    const printifyRes = await fetch("https://api.printify.com/v1/shops/5414762/products.json", {
-        method: 'GET',
-        headers: {"Authorization": `Bearer ${process.env.PRINTIFY_TOKEN_READ_ONLY}`}
-    })
-    const data = await printifyRes.json()
-    res.status(200).json(data);
+    // const printifyRes = await fetch("https://api.printify.com/v1/shops/5414762/products.json", {
+    //     method: 'GET',
+    //     headers: {"Authorization": `Bearer ${process.env.PRINTIFY_TOKEN_READ_ONLY}`}
+    // })
+    // const data = await printifyRes.json()
+    // res.status(200).json(data);
+    res.status(200).json({"status": "denied"});
 }
--- client/pages/api/revalidate.tsx
+++ client/pages/api/revalidate.tsx
@@ -2,8 +2,9 @@
 // https://nextjs.org/docs/basic-features/data-fetching/incremental-static-regeneration#using-on-demand-revalidation
 
 export default async function handler(req, res) {
-  for (const url of req.body) {
-    await res.unstable_revalidate(url); //url will be in body 
-  }
-  res.status(200).json({ revalidate: true });
+  // for (const url of req.body) {
+  //   await res.unstable_revalidate(url); //url will be in body 
+  // }
+  // res.status(200).json({ revalidate: true });
+  res.status(200).json({"status": "denied"});
 }
